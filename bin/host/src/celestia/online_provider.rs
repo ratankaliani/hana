39f06c7a864ea5aa47d4edbc02b12f4677c603a6
Diego <diego@celestia.org>
2025-03-07 13:10:37 -0500

    cleanup

--- bin/host/src/celestia/online_provider.rs
+++ bin/host/src/celestia/online_provider.rs
@@ -1,9 +1,13 @@
-use alloy_primitives::{keccak256, Address, B256, U256};
+use alloy_primitives::{keccak256, Address, Bytes, B256, U256};
 use alloy_provider::{Provider, RootProvider};
 use alloy_rpc_types::{BlockNumberOrTag, Filter, FilterBlockOption, FilterSet};
 use alloy_sol_types::{sol, SolEvent};
-use celestia_rpc::Client;
-use celestia_types::nmt::Namespace;
+use celestia_rpc::{blobstream::BlobstreamClient, Client, HeaderClient, ShareClient};
+use celestia_types::{nmt::Namespace, Blob};
+use hana_oracle::payload::{
+    calculate_mapping_slot, encode_data_root_tuple, verify_data_commitment_storage, OraclePayload,
+    DATA_COMMITMENTS_SLOT,
+};
 use std::sync::Arc;
 use tracing::info;
 
@@ -154,6 +158,100 @@ impl OnlineCelestiaProvider {
             } else {
                 0
             };
+        }
+    }
+
+    pub async fn generate_oracle_payload(
+        &self,
+        l1_provider: &RootProvider,
+        height: u64,
+        blob: Blob,
+    ) -> Result<OraclePayload, anyhow::Error> {
+        // Fetch the block's data root
+        let header = self.client.header_get_by_height(height).await?;
+
+        let data_root = header.dah.hash();
+
+        let eds_row_roots = header.dah.row_roots();
+        let eds_size: u64 = eds_row_roots.len().try_into().unwrap();
+        let ods_size: u64 = eds_size / 2;
+
+        let index = blob.index.unwrap();
+        let first_row_index: u64 = index.div_ceil(eds_size) - 1;
+        let start_index = blob.index.unwrap() - (first_row_index * ods_size);
+        let end_index = start_index + blob.shares_len() as u64;
+
+        let share_proof = self
+            .client
+            .share_get_range(&header, start_index, end_index)
+            .await
+            .expect("Failed getting share proof")
+            .proof;
+
+        // validate the proof before placing it on the KV store
+        share_proof
+            .verify(data_root)
+            .expect("failed to verify share proof against data root");
+
+        let event = self
+            .find_data_commitment(height, l1_provider)
+            .await
+            .unwrap();
+
+        let data_root_proof = self
+            .client
+            .get_data_root_tuple_inclusion_proof(height, event.start_block, event.end_block)
+            .await?;
+
+        let encoded_data_root_tuple = encode_data_root_tuple(height, &data_root);
+
+        data_root_proof
+            .verify(encoded_data_root_tuple, *event.data_commitment.clone())
+            .expect("failed to verify data root tuple inclusion proof");
+
+        let slot = calculate_mapping_slot(DATA_COMMITMENTS_SLOT, event.proof_nonce);
+
+        let slot_b256 = B256::from_slice(slot.as_slice());
+
+        let proof_response = l1_provider
+            .get_proof(self.blobstream_address, vec![slot_b256])
+            .await?;
+
+        let proof_bytes: Vec<Bytes> = proof_response
+            .storage_proof
+            .into_iter()
+            .flat_map(|proof| {
+                // Extract the proof field and apply any needed transformations
+                proof.proof.into_iter().map(|bytes| {
+                    // You can apply transformations here if needed
+                    // For example: Bytes::from(some_transformation(bytes))
+                    // But in this case, we can just return the bytes directly
+                    bytes
+                })
+            })
+            .collect();
+
+        match verify_data_commitment_storage(
+            proof_response.storage_hash,
+            proof_bytes.clone(),
+            event.proof_nonce,
+            event.data_commitment,
+        ) {
+            Ok(_) => {
+                println!("Succesfully verified storage proof for Blobstream data commitment");
+
+                return Ok(OraclePayload::new(
+                    Bytes::from(blob.data),
+                    data_root,
+                    event.data_commitment,
+                    data_root_proof,
+                    share_proof,
+                    event.proof_nonce,
+                    proof_response.storage_hash.clone(),
+                    proof_bytes,
+                ));
+            }
+            Err(err) => anyhow::bail!("Error verifying storage proof {}", err),
         }
     }
 }
